ee195c74f21cc5c94363f5c947e4523348f05f4a
mattrbanks <banks.matt81@gmail.com>
2020-12-18 12:10:54 -0500

    Fixed file structure and config. Deleted files that are not needed.

--- index.php
+++ index.php
@@ -1,13 +1,36 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title>Document</title>
-  </head>
+<?php
+require('admin/config/config.php');
+require('admin/config/db.php');
 
-  <body>
-    <h1>Hello World!</h1>
-  </body>
-</html>
+// Create Query
+$query = 'SELECT * FROM posts ORDER BY created_at DESC';
+
+// Get Result
+$result = mysqli_query($conn, $query);
+
+// Fetch Data
+$posts = mysqli_fetch_all($result, MYSQLI_ASSOC);
+var_dump($posts); //show raw data
+
+// Free Result
+mysqli_free_result($result);
+
+// Close Connection
+mysqli_close($conn);
+?>
+
+<?php include('inc/header.php'); ?>
+<div class="container">
+  <h1>Posts</h1>
+  <?php foreach ($posts as $post) : ?>
+    <div class="card p-3">
+      <h3><?php echo $post['title']; ?></h3>
+      <small>Created on <?php echo $post['created_at']; ?> by <?php echo $post['author']; ?></small>
+      <p><?php echo $post['body']; ?></p>
+      <div>
+        <a class="btn btn-primary" href="<?php echo ROOT_URL; ?>post.php?id=<?php echo $post['id']; ?>">Read More</a>
+      </div>
+    </div>
+  <?php endforeach; ?>
+</div>
+<?php include('inc/footer.php'); ?>
